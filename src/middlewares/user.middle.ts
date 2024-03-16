94fa328dac700a1eadfe5f118fe619658320295d
michaelletichevsky <maslo@dlit.dp.ua>
2024-03-16 20:37:01 +0200

    cors fix

--- src/middlewares/user.middle.ts
+++ src/middlewares/user.middle.ts
@@ -11,8 +11,8 @@ export class UserMiddleware implements NestMiddleware {
   ){}
   async use(req: IRequest, res: Response, next: NextFunction) {
     console.log(req.cookies)
-    if(req.cookies && req.cookies['token']){
-      let token = req.cookies['token'];
+    if(req.headers && req.headers['auth']){
+      let token: string = req.headers['auth'].toString();
       let user: UserToken | null = await this.userService.verifyToken(token)
       req.user = this.userService.getUser(user.username);
     }
