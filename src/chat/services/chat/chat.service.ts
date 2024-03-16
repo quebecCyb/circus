bc63a9385379e4e7ff44b1e574d836d42350ba17
Никита <65460812+N1k1ta-white@users.noreply.github.com>
2024-03-16 23:28:26 +0200

    added connect/disconnect

--- src/chat/services/chat/chat.service.ts
+++ src/chat/services/chat/chat.service.ts
@@ -1,4 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from "@nestjs/common";
+import { Socket } from "socket.io";
 
 @Injectable()
-export class ChatService {}
+export class ChatService {
+  private readonly nameToSocket: Map<string, Socket> = new Map();
+
+  connect(name: string, socketUser: Socket): void {
+    if (this.nameToSocket.has(name)) {
+      throw new ConflictException(`User with name ${name} is already connected`);
+    }
+    this.nameToSocket.set(name, socketUser);
+  }
+
+  disconnect(name: string): void {
+    if (!this.nameToSocket.has(name)) {
+      throw new ConflictException(`User with name ${name} is not connected`);
+    }
+    this.nameToSocket.delete(name);
+  }
+}
