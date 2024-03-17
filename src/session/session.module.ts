91230f9f4901702e65f34258ab2fea1e9203b51b
michaelletichevsky <maslo@dlit.dp.ua>
2024-03-17 06:37:15 +0200

    game logic

--- src/session/session.module.ts
+++ src/session/session.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SessionController } from './controllers/session/session.controller';
 import { SessionService } from './services/session/session.service';
+import { ChatGateway } from 'src/chat/chat.gateway';
 
 @Module({
+  imports: [ChatGateway],
   controllers: [SessionController],
   providers: [SessionService],
   exports: [SessionService]
